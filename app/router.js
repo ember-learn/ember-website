7318df88c630ad4ef61c0bfe8c61a36ce55d0734
Chris Manson <chris@stonecircle.io>
2018-05-28 13:04:17 +0100

    adding initial routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,17 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('community', function() {
+    this.route('meetups');
+    this.route('meetups-getting-started');
+  });
+  this.route('learn', function() {
+    this.route('examples');
+  });
+  this.route('security');
+  this.route('sponsors');
+  this.route('meetup-assets');
+  this.route('mascots');
 });
 
 export default Router;
