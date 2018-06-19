d08e34a0acd403d16ee78c90ec0ec368762e3e9f
Chris Manson <chris@stonecircle.io>
2018-06-19 14:35:55 +0100

    first pass on the learn page

---
+++ app/routes/learn.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default Route.extend({
+  ajax: service(),
+  model() {
+    return this.ajax.request('/data/showcase.json');
+  }
+});
