d1b6d81dd797e5318c9760cc4e3af9550c27cab6
Abhilash L R <abhilashlr.789@gmail.com>
2018-10-28 12:57:09 +0100

    Meetups page

--- app/routes/community/meetups.js
+++ app/routes/community/meetups.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
+import fetch from 'ember-fetch/ajax';
+import config from 'ember-website/config/environment';
 
 export default Route.extend({
+  model() {
+    return fetch(`${config.host}/data/meetups.json`);
+  }
 });
