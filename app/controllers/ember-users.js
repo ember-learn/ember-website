4f71370f6eae4034ec9962bc91baef3f82f46b78
Chris Manson <chris.manson@simplabs.com>
2018-11-12 22:42:39 +0000

    trying to fix dynamic asset render

--- app/controllers/ember-users.js
+++ app/controllers/ember-users.js
@@ -1,7 +1,10 @@
 import Controller from '@ember/controller';
 import { sort } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  fastboot: service(),
+
   sortingKey: Object.freeze(['featured:desc', 'added']),
   sortedModel: sort('model', 'sortingKey')
 })
