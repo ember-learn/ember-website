5a8bb3c2c38d4adeb88fb62434b35a63490f47ca
Chris Manson <chris@manson.ie>
2018-12-25 09:19:28 +0000

    improving the team-member blueprint

--- blueprints/team-member/files/data/team/__name__.md
+++ blueprints/team-member/files/data/team/__name__.md
@@ -5,6 +5,7 @@ last:
 github:
 twitter:
 image:
+added: <%= date %>
 teams:
 # pick from the following - you can just remove the # and delete the other lines
 #  - corejs
--- blueprints/team-member/index.js
+++ blueprints/team-member/index.js
@@ -1,4 +1,10 @@
 /* eslint-env node */
 module.exports = {
-  description: 'Simple Blueprint to add an Ember Team-Member'
+  description: 'Simple Blueprint to add an Ember Team-Member',
+
+  locals() {
+    return {
+      date: (new Date()).toISOString()
+    };
+  }
 };
