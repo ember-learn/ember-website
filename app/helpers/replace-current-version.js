a5bb336b269139238d230333682032d7fa119a15
Ricardo Mendes <rokusu@gmail.com>
2018-12-28 16:12:19 +0000

    clean up replace-current-version and learn-showcase

--- app/helpers/replace-current-version.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { htmlSafe } from '@ember/template';
-
-export function replaceCurrentVersion([input]/*, hash*/) {
-  return htmlSafe(input.replace("_CURRENT_VERSION_", "3.6"));
-}
-
-export default helper(replaceCurrentVersion);
