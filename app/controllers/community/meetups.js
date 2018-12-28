36b2e80e7b3e300823000a9204bc7d6cd090b06b
Chris Manson <chris@manson.ie>
2018-12-28 12:42:00 +0000

    adding map implementation with ember-leaflet

--- app/controllers/community/meetups.js
+++ app/controllers/community/meetups.js
@@ -2,5 +2,8 @@ import Controller from '@ember/controller';
 import groupBy from 'ember-group-by';
 
 export default Controller.extend({
+  lat: 20,
+  lng: 0,
+  zoom: 2,
   meetupsByArea: groupBy('model', 'area')
 });
