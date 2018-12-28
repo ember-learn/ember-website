f751e4f9accdb41b284ef0530d0c1a84d6299bbf
Ricardo Mendes <rokusu@gmail.com>
2018-12-28 15:43:48 +0000

    implement basic test for replace-current-version helper

--- tests/integration/helpers/replace-current-version-test.js
+++ tests/integration/helpers/replace-current-version-test.js
@@ -8,10 +8,10 @@ module('Integration | Helper | replace-current-version', function(hooks) {
 
   // Replace this with your real tests.
   test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+    this.set('inputValue', 'ONE _CURRENT_VERSION_ TWO');
 
     await render(hbs`{{replace-current-version inputValue}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), 'ONE 3.6 TWO');
   });
 });
