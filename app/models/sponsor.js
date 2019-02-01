f8e3dd6cb367bde6cde683402fa6a5e0711780a5
Chris Manson <chris@manson.ie>
2019-02-01 09:53:12 +0000

    add sponsors route

---
+++ app/models/sponsor.js
@@ -0,0 +1,29 @@
+import DS from 'ember-data';
+import { computed } from '@ember/object';
+
+export default DS.Model.extend({
+  content: DS.attr(),
+  end: DS.attr('date'),
+  image: DS.attr(),
+  start: DS.attr('date'),
+  url: DS.attr(),
+
+  term: computed('start', 'end', function() {
+    let startYear = this.start.getFullYear();
+    let endYear;
+
+    if(this.end) {
+      endYear = this.end.getFullYear();
+    }
+
+    if(startYear === endYear) {
+      return startYear;
+    }
+
+    if(!endYear) {
+      endYear = 'present';
+    }
+
+    return `${startYear} - ${endYear}`;
+  })
+});
