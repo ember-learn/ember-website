89f9bee42e6e48fd620e8d4030ea6dc2d9adc318
Nana Adjei Manu <claeusdev@gmail.com>
2019-02-03 11:29:41 +0000

    complete functionality

--- app/routes/community/projects/index.js
+++ app/routes/community/projects/index.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        id: 1,
+        title: 'Project Title',
+        link: 'www.example.com',
+        description: 'lets describe this project',
+        thumbnail: 'https://placeimg.com/640/480/any',
+        date_added: '2010/12/08'
+      },
+      {
+        id: 2,
+        title: 'Project Title',
+        link: 'www.example.com',
+        description: 'lets describe this project',
+        thumbnail: 'https://placeimg.com/640/480/any',
+        date_added: '2010/12/08'
+      }
+    ];
+  }
 });
