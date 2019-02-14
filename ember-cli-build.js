251fc2f2ef0d1c7f0dfc42864228127b7129bdb8
Chris Manson <chris@manson.ie>
2019-02-14 22:25:54 +0000

    porting statusboard code

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -61,18 +61,12 @@ module.exports = function(defaults) {
     },
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import("node_modules/highlightjs/highlight.pack.js");
+  app.import('vendor/shims/highlightjs.js');
+
+  app.import("node_modules/markdown-it/dist/markdown-it.js", {
+    using: [{ transformation: "amd", as: "markdown-it" }]
+  });
 
   return app.toTree();
 };
