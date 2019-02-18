04c47af1b16aac27fb95e8fdbb3e86616a5a4a9d
Alexey Kulakov <uni_que@me.com>
2019-02-18 05:33:59 -0800

    home page is finished

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,41 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return {
+      users: [{
+        url: 'https://www.microsoft.com',
+        file: 'microsoft.png',
+        alt: 'Microsoft logo',
+      }, {
+        url: 'https://www.intercom.com',
+        file: 'intercom.png',
+        alt: 'Intercom logo',
+      }, {
+        url: 'https://www.netflix.com',
+        file: 'netflix.png',
+        alt: 'Netflix logo',
+      }, {
+        url: 'https://www.heroku.com',
+        file: 'heroku.png',
+        alt: 'Heroku logo',
+      }, {
+        url: 'https://www.skylight.io',
+        file: 'skylight.png',
+        alt: 'Skylight logo',
+      }, {
+        url: 'https://www.linkedin.com',
+        file: 'linkedin.png',
+        alt: 'Linkedin logo',
+      }, {
+        url: 'https://www.square.com',
+        file: 'square.png',
+        alt: 'Square logo',
+      }, {
+        url: 'https://www.zendesk.com',
+        file: 'zendesk.png',
+        alt: 'Zendesk logo',
+      }]
+    };
+  }
 });
---
+++ tests/integration/components/job-board-banner-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | job-board-banner', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{job-board-banner}}`);
+
+    assert.equal(this.element.querySelector('.job-board-banner').innerHTML.trim(), 'Post and find jobs on the Ember job board');
+  });
+});
