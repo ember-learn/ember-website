e2b25dd4dfb5c38113e392333e9510012245a162
Alexey Kulakov <uni_que@me.com>
2019-02-18 06:09:34 -0800

    fixing test

--- tests/integration/components/job-board-banner-test.js
+++ tests/integration/components/job-board-banner-test.js
@@ -9,8 +9,9 @@ module('Integration | Component | job-board-banner', function(hooks) {
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('showBanner', true);
 
-    await render(hbs`{{job-board-banner}}`);
+    await render(hbs`{{job-board-banner showBanner=showBanner}}`);
 
     assert.equal(this.element.querySelector('.job-board-banner').innerHTML.trim(), 'Post and find jobs on the Ember job board');
   });
