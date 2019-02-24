31071468e3fcd97f179a76d2db36bc613fed6c0a
Alexey Kulakov <uni_que@me.com>
2019-02-24 05:14:25 -0800

    setting up charts for 2016

---
+++ app/highchart-themes/survey.js
@@ -0,0 +1,16 @@
+export default {
+  chart: {
+    type: 'column',
+  },
+  title: {
+    text: '',
+  },
+  yAxis: {
+    title: {
+      text: null,
+    },
+    labels: {
+      format: '{value} %',
+    },
+  },
+};
