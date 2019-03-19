ff9681339d07d7e946a57c00d682ba2d4aea79dd
Melanie Sumner <melaniersumner@gmail.com>
2019-03-18 17:25:47 -0700

    Revert "Markdown survey prose"

--- app/routes/ember-community-survey-2019.js
+++ app/routes/ember-community-survey-2019.js
@@ -2,8 +2,4 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   classNames: ['ember-community-survey'],
-
-  model() {
-    return this.store.findAll('survey-2019');
-  }
 });
--- data/survey-2019/05-stackSkillsChart.md
+++
@@ -1,6 +0,0 @@
----
-title: Stack Skillset Self-Appraisal
-question-id: stackSkillsChart
-fragment: stack-skills
----
-This is placeholder text.
