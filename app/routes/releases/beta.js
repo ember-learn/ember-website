d72b325e655a2321e10e7b9eae787fb6ef60f1ab
Chris Manson <chris@manson.ie>
2019-04-03 11:20:04 +0100

    using data instead of fixutres

---
+++ app/routes/releases/beta.js
@@ -0,0 +1,11 @@
+import { hash } from 'rsvp';
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model() {
+    return hash({
+      ember: this.store.find('project', 'ember/beta'),
+      emberData: this.store.find('project', 'emberData/beta')
+    });
+  }
+});
