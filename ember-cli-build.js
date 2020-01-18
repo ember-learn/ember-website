789b3298ddabd2306f969cb56d183a6628a7cea7
Chris Manson <chris@manson.ie>
2020-01-18 00:26:20 +0000

    dynamically load highcharts (to reduce initial asset size)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    babel: {
+      plugins: [ require.resolve('ember-auto-import/babel-plugin') ]
+    },
     ifa: {
       enabled: true,
       inline: true,
