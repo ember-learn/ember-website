9bb4abc370a9057283f9ee043cccd05df43fc517
Braden Lawrence <b@bradenlawrence.com>
2020-10-17 11:51:13 -0400

    remove comments from linter rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -9,14 +9,7 @@ module.exports = {
       The recommended rules are failing. Let's fix the errors
       and remove the rule exceptions one by one.
     */
-    // 'link-rel-noopener': false,
-    'no-action': false,
-    // 'no-curly-component-invocation': false,
+    'no-action': true,
     'no-html-comments': false,
-    // 'no-implicit-this': false,
-    // 'no-obsolete-elements': false,
-    // 'no-positive-tabindex': false,
-    // 'require-button-type': false,
-    // 'require-valid-alt-text': false,
   },
 };
