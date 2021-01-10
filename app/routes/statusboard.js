a235a362148b1b9eb3733f4534a5537bf3d1c0f8
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-10 17:03:21 +0100

    Removed unused code

--- app/routes/statusboard.js
+++ app/routes/statusboard.js
@@ -1,7 +1,16 @@
 import Route from '@ember/routing/route';
 
+/*
+  @deprecated
+
+  From .netlifyredirects, we see that, when a user visits /statusboard,
+  they will be redirected to an external webpage.
+
+  Consider removing this route file. In case the route file is needed,
+  I only commented out the model hook for now.
+*/
 export default Route.extend({
-  model() {
-    return this.store.findAll('feature');
-  }
+  // model() {
+  //   return this.store.findAll('feature');
+  // }
 });
