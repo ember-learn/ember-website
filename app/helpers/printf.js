1e89616682b94678bf0a19a027fe1ce30195450a
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-11 18:10:25 +0100

    Simplified code for {{printf}} helper

--- app/helpers/printf.js
+++ app/helpers/printf.js
@@ -1,7 +1,5 @@
 import { helper } from '@ember/component/helper';
 
-export function printf([template, value] /*, hash*/) {
-  return template.replace(/%s/g, value);
-}
-
-export default helper(printf);
+export default helper(function printf([string, replacement]) {
+  return string.replace(/%s/g, replacement);
+});
