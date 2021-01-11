90cd2a4ffe14a38ae90fc9c4055bbc6673e8a8e2
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-11 17:43:07 +0100

    Simplified code for {{format-date-time}} helper

--- app/helpers/format-date-time.js
+++ app/helpers/format-date-time.js
@@ -1,9 +1,6 @@
 import { helper } from '@ember/component/helper';
 import { DateTime } from 'luxon';
 
-export function formatDateTime([date, format]) {
-  let defaultFormat = 'MMM d';
-  return DateTime.fromJSDate(date).toFormat(format ? format : defaultFormat);
-}
-
-export default helper(formatDateTime);
+export default helper(function formatDateTime([date, format = 'MMM d']) {
+  return DateTime.fromJSDate(date).toFormat(format);
+});
