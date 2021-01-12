6e215ddb3f4bc72f20c86ee74efe47d34aaac5ae
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-12 16:12:56 +0100

    Extracted fixture data for the project model

--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -1,4 +1,5 @@
 import { setupTest } from 'ember-qunit';
+import { getAttributesForId } from 'ember-website/mirage/data/projects';
 import { module, test } from 'qunit';
 
 module('Unit | Model | project', function (hooks) {
@@ -10,26 +11,8 @@ module('Unit | Model | project', function (hooks) {
     });
 
     test('The model can describe an LTS release', function (assert) {
-      const model = this.store.createRecord('project', {
-        baseFileName: 'ember',
-        changelogPath: 'CHANGELOG.md',
-        channel: 'lts',
-        content:
-          '# Install Ember %s:\n<br>\nnpm install --save-dev ember-source@~%s',
-        cycleEstimatedFinishDate: undefined,
-        date: new Date('2020-09-09'),
-        debugFileName: '.debug.js',
-        filter: ['/ember\\./', '/ember-template-compiler/'],
-        finalVersion: undefined,
-        futureVersion: '3.20.7',
-        id: 'ember/lts',
-        ignoreFiles: ['ember.js'],
-        initialVersion: '3.20.0',
-        lastRelease: '3.20.6',
-        name: 'Ember',
-        nextDate: undefined,
-        repo: 'emberjs/ember.js',
-      });
+      const attributes = getAttributesForId('ember/lts');
+      const model = this.store.createRecord('project', attributes);
 
       assert.ok(model, 'We can create the record.');
 
@@ -47,26 +30,8 @@ module('Unit | Model | project', function (hooks) {
     });
 
     test('The model can describe a stable release', function (assert) {
-      const model = this.store.createRecord('project', {
-        baseFileName: 'ember',
-        changelogPath: 'CHANGELOG.md',
-        channel: 'release',
-        content:
-          '# Install Ember %s:\n<br>\nnpm install --save-dev ember-source@~%s',
-        cycleEstimatedFinishDate: undefined,
-        date: new Date('2021-01-07'),
-        debugFileName: '.debug.js',
-        filter: ['/ember\\./', '/ember-template-compiler/'],
-        finalVersion: undefined,
-        futureVersion: '3.24.1',
-        id: 'ember/release',
-        ignoreFiles: ['ember.js'],
-        initialVersion: '3.24.0',
-        lastRelease: '3.24.0',
-        name: 'Ember',
-        nextDate: undefined,
-        repo: 'emberjs/ember.js',
-      });
+      const attributes = getAttributesForId('ember/release');
+      const model = this.store.createRecord('project', attributes);
 
       assert.ok(model, 'We can create the record.');
 
@@ -84,26 +49,8 @@ module('Unit | Model | project', function (hooks) {
     });
 
     test('The model can describe a beta release', function (assert) {
-      const model = this.store.createRecord('project', {
-        baseFileName: 'ember',
-        changelogPath: 'CHANGELOG.md',
-        channel: 'beta',
-        content:
-          '# Install Ember %s:\n<br>\nnpm install --save-dev ember-source@~%s',
-        cycleEstimatedFinishDate: new Date('2021-02-08'),
-        date: new Date('2020-12-28'),
-        debugFileName: '.debug.js',
-        filter: ['/ember\\./', '/ember-template-compiler/'],
-        finalVersion: '3.25.0',
-        futureVersion: '3.25.0-beta.2',
-        id: 'ember/beta',
-        ignoreFiles: ['ember.js'],
-        initialVersion: '3.24.0',
-        lastRelease: '3.25.0-beta.1',
-        name: 'Ember',
-        nextDate: new Date('2021-02-08'),
-        repo: 'emberjs/ember.js',
-      });
+      const attributes = getAttributesForId('ember/beta');
+      const model = this.store.createRecord('project', attributes);
 
       assert.ok(model, 'We can create the record.');
 
@@ -121,25 +68,8 @@ module('Unit | Model | project', function (hooks) {
     });
 
     test('The model can describe a canary release', function (assert) {
-      const model = this.store.createRecord('project', {
-        baseFileName: 'ember',
-        changelogPath: undefined,
-        channel: 'canary',
-        content: '',
-        cycleEstimatedFinishDate: undefined,
-        date: undefined,
-        debugFileName: '.debug.js',
-        filter: ['/ember\\./', '/ember-template-compiler/'],
-        finalVersion: undefined,
-        futureVersion: undefined,
-        id: 'ember/canary',
-        ignoreFiles: ['ember.js'],
-        initialVersion: undefined,
-        lastRelease: undefined,
-        name: 'Ember',
-        nextDate: undefined,
-        repo: 'emberjs/ember.js',
-      });
+      const attributes = getAttributesForId('ember/canary');
+      const model = this.store.createRecord('project', attributes);
 
       assert.ok(model, 'We can create the record.');
 
@@ -163,26 +93,8 @@ module('Unit | Model | project', function (hooks) {
     });
 
     test('The model can describe a stable release', function (assert) {
-      const model = this.store.createRecord('project', {
-        baseFileName: 'ember-data',
-        changelogPath: 'CHANGELOG.md',
-        channel: 'release',
-        content:
-          '# Install Ember-Data %s:\n<br>\nnpm install --save-dev ember-data@%s',
-        cycleEstimatedFinishDate: undefined,
-        date: new Date('2021-01-07'),
-        debugFileName: '.js',
-        filter: ['/ember-data\\./'],
-        finalVersion: undefined,
-        futureVersion: '3.24.1',
-        id: 'emberData/release',
-        ignoreFiles: undefined,
-        initialVersion: '3.24.0',
-        lastRelease: '3.24.0',
-        name: 'Ember Data',
-        nextDate: undefined,
-        repo: 'emberjs/data',
-      });
+      const attributes = getAttributesForId('emberData/release');
+      const model = this.store.createRecord('project', attributes);
 
       assert.ok(model, 'We can create the record.');
 
@@ -200,26 +112,8 @@ module('Unit | Model | project', function (hooks) {
     });
 
     test('The model can describe a beta release', function (assert) {
-      const model = this.store.createRecord('project', {
-        baseFileName: 'ember-data',
-        changelogPath: 'CHANGELOG.md',
-        channel: 'beta',
-        content:
-          '# Install Ember-Data %s:\n<br>\nnpm install --save-dev ember-data@%s',
-        cycleEstimatedFinishDate: undefined,
-        date: new Date('2021-01-05'),
-        debugFileName: '.js',
-        filter: ['/ember-data\\./'],
-        finalVersion: '3.25.0',
-        futureVersion: '3.25.0-beta.1',
-        id: 'emberData/beta',
-        ignoreFiles: ['ember.js'],
-        initialVersion: undefined,
-        lastRelease: '3.25.0-beta.0',
-        name: 'Ember Data',
-        nextDate: undefined,
-        repo: 'emberjs/data',
-      });
+      const attributes = getAttributesForId('emberData/beta');
+      const model = this.store.createRecord('project', attributes);
 
       assert.ok(model, 'We can create the record.');
 
@@ -237,26 +131,8 @@ module('Unit | Model | project', function (hooks) {
     });
 
     test('The model can describe a canary release', function (assert) {
-      const model = this.store.createRecord('project', {
-        baseFileName: 'ember-data',
-        changelogPath: undefined,
-        channel: 'canary',
-        content:
-          '# Install the latest Ember-Data canary:\n<br>\nnpm install --save-dev emberjs/data#master',
-        cycleEstimatedFinishDate: undefined,
-        date: undefined,
-        debugFileName: '.js',
-        filter: ['/ember-data\\./'],
-        finalVersion: undefined,
-        futureVersion: undefined,
-        id: 'emberData/canary',
-        ignoreFiles: undefined,
-        initialVersion: undefined,
-        lastRelease: undefined,
-        name: 'Ember Data',
-        nextDate: undefined,
-        repo: 'emberjs/data',
-      });
+      const attributes = getAttributesForId('emberData/canary');
+      const model = this.store.createRecord('project', attributes);
 
       assert.ok(model, 'We can create the record.');
 
