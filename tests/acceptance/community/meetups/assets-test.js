73845c149305fb11128361b6584ca4db18dcd641
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-12 07:51:47 +0100

    Loaded fixture data in all application tests

--- tests/acceptance/community/meetups/assets-test.js
+++ tests/acceptance/community/meetups/assets-test.js
@@ -1,13 +1,20 @@
 import { visit } from '@ember/test-helpers';
 import percySnapshot from '@percy/ember';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupApplicationTest } from 'ember-qunit';
+import loadDefaultScenario from 'ember-website/mirage/scenarios/default';
 import { setupPageTitleTest } from 'ember-website/tests/helpers/page-title';
 import { module, test } from 'qunit';
 
 module('Acceptance | community/meetups/assets', function (hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
   setupPageTitleTest(hooks);
+
+  hooks.beforeEach(function () {
+    loadDefaultScenario(this.server);
+  });
 
   test('Percy snapshot', async function (assert) {
     await visit('/community/meetups/assets');
