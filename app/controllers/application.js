86a0b37494008c0f69a230deff533d65dffce61e
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-14 11:02:50 +0100

    Reordered import statements

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
-import styleguideLinks from 'ember-styleguide/constants/links';
 import { infoLinks } from 'ember-styleguide/constants/es-footer';
+import styleguideLinks from 'ember-styleguide/constants/links';
 import replaceLinks from 'ember-website/utils/replace-links';
 
 export default Controller.extend({
