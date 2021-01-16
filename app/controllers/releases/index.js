a6728c4f1ddbf58ce8b0d7a16e2ca07db89ba776
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-16 09:30:28 +0100

    Removed unused code in releases.index controller

--- app/controllers/releases/index.js
+++ app/controllers/releases/index.js
@@ -5,14 +5,4 @@ export default Controller.extend({
   emberBetaProject: computed('model.@each.isEmberBeta', function () {
     return this.model.find((p) => p.isEmberBeta);
   }),
-  emberReleaseProject: computed('model.@each.{channel,name}', function () {
-    return this.model.find((p) => {
-      return p.get('channel') === 'release' && p.get('name') === 'Ember';
-    });
-  }),
-  emberLTSProject: computed('model.@each.{channel,name}', function () {
-    return this.model.find((p) => {
-      return p.get('channel') === 'lts' && p.get('name') === 'Ember';
-    });
-  }),
 });
