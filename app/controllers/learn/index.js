cd97c9fbaa9889d43c76fda993f014d2f71e8496
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-16 09:25:06 +0100

    Removed learn.index controller (unused)

--- app/controllers/learn/index.js
+++
@@ -1,7 +0,0 @@
-import Controller from '@ember/controller';
-import { sort } from '@ember/object/computed';
-
-export default Controller.extend({
-  sortingKey: Object.freeze(['id']),
-  sortedModel: sort('model', 'sortingKey'),
-});
