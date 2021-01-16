0347a0285a8876c9c6726d6193af5daf0ca1220e
Devin Weaver <suki@tritarget.org>
2021-01-16 10:32:42 -0500

    Refactor team controller to native class
    The classic syntax used a helper function to define computed properties
    on the controller. I converted this to a custom decorator instead.

--- app/controllers/team.js
+++ app/controllers/team.js
@@ -1,21 +1,25 @@
 import Controller from '@ember/controller';
+/* eslint-disable-next-line ember/no-computed-properties-in-native-classes */
 import { sort } from '@ember/object/computed';
-import { computed } from '@ember/object';
 
 function inTeam(team) {
-  return computed('sortedModel.[]', function () {
-    return this.sortedModel.filter((member) => member.teams.includes(team));
-  });
+  return (target, key) => {
+    Object.defineProperty(target, key, {
+      get() {
+        return this.sortedModel.filter((member) => member.teams.includes(team));
+      },
+    });
+  };
 }
 
-export default Controller.extend({
-  sortingKeys: Object.freeze(['last', 'first']),
-  sortedModel: sort('model', 'sortingKeys'),
+export default class TeamController extends Controller {
+  sortingKeys = ['last', 'first'];
 
-  alumniTeamMembers: inTeam('alumni'),
-  coreCLITeamMembers: inTeam('cli'),
-  coreTeamMembers: inTeam('corejs'),
-  dataTeamMembers: inTeam('data'),
-  learningTeamMembers: inTeam('learning'),
-  steeringCommitteeMembers: inTeam('steering'),
-});
+  @sort('model', 'sortingKeys') sortedModel;
+  @inTeam('alumni') alumniTeamMembers;
+  @inTeam('cli') coreCLITeamMembers;
+  @inTeam('corejs') coreTeamMembers;
+  @inTeam('data') dataTeamMembers;
+  @inTeam('learning') learningTeamMembers;
+  @inTeam('steering') steeringCommitteeMembers;
+}
