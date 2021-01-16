65d4fb1e047833974a8a350b6a8c2d848f7e7dea
Devin Weaver <suki@tritarget.org>
2021-01-16 17:11:39 -0500

    Refactor mascots.index controller to native class

--- app/controllers/mascots/index.js
+++ app/controllers/mascots/index.js
@@ -1,7 +1,8 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  queryParams: ['filter', 'display'],
-  filter: 'all',
-  display: 'small',
-});
+export default class MascotsIndexController extends Controller {
+  queryParams = ['filter', 'display'];
+  @tracked filter = 'all';
+  @tracked display = 'small';
+}
