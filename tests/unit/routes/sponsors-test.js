1f6e92850a22337e92074d291ca12d3427ec5390
ijlee2 <ijlee2@users.noreply.github.com>
2021-01-16 11:29:54 +0100

    Added unit tests for the model hook

--- tests/unit/routes/sponsors-test.js
+++ tests/unit/routes/sponsors-test.js
@@ -1,12 +1,27 @@
+import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupTest } from 'ember-qunit';
+import sponsors from 'ember-website/mirage/data/sponsors';
 import { module, test } from 'qunit';
 
 module('Unit | Route | sponsors', function (hooks) {
   setupTest(hooks);
+  setupMirage(hooks);
 
-  test('it exists', function (assert) {
+  hooks.beforeEach(function () {
+    this.server.db.loadData({ sponsors });
+  });
+
+  test('The model hook returns all sponsors', async function (assert) {
     const route = this.owner.lookup('route:sponsors');
+    const model = await route.model();
 
-    assert.ok(route);
+    // Create an intermediate data structure for assertion
+    const output = model.map(({ id }) => id);
+
+    assert.deepEqual(
+      output,
+      sponsors.map(({ id }) => id),
+      'We get the correct output.'
+    );
   });
 });
--- tests/unit/routes/team-test.js
+++ tests/unit/routes/team-test.js
@@ -1,12 +1,27 @@
+import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupTest } from 'ember-qunit';
+import teamMembers from 'ember-website/mirage/data/team-members';
 import { module, test } from 'qunit';
 
 module('Unit | Route | team', function (hooks) {
   setupTest(hooks);
+  setupMirage(hooks);
 
-  test('it exists', function (assert) {
+  hooks.beforeEach(function () {
+    this.server.db.loadData({ teamMembers });
+  });
+
+  test('The model hook returns all team members', async function (assert) {
     const route = this.owner.lookup('route:team');
+    const model = await route.model();
 
-    assert.ok(route);
+    // Create an intermediate data structure for assertion
+    const output = model.map(({ id }) => id);
+
+    assert.deepEqual(
+      output,
+      teamMembers.map(({ id }) => id),
+      'We get the correct output.'
+    );
   });
 });
