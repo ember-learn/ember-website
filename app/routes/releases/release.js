8ed152d74b07cfef41a1e1e595965feae0099de6
Paweł Kuwik <p.kuwik@selleo.com>
2021-01-18 19:26:26 +0100

    refactor releases release route to native class

--- app/routes/releases/release.js
+++ app/routes/releases/release.js
@@ -1,11 +1,11 @@
 import { hash } from 'rsvp';
 import Route from '@ember/routing/route';
 
-export default Route.extend({
+export default class ReleasesReleaseRoute extends Route {
   model() {
     return hash({
       ember: this.store.find('project', 'ember/release'),
       emberData: this.store.find('project', 'emberData/release'),
     });
-  },
-});
+  }
+}
