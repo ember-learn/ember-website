96feb23a27bee8cc33353edfb8d8e68fbc3023fe
Paweł Kuwik <p.kuwik@selleo.com>
2021-01-18 19:21:00 +0100

    refactor team route to native class

--- app/routes/team.js
+++ app/routes/team.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
+export default class TeamRoute extends Route {
   model() {
     return this.store.findAll('team-member');
-  },
-});
+  }
+}
