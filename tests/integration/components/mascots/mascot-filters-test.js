dc771a8ffbfd8f4cf1021f5b04a3ef4a54bbaeda
Isaac Lee <ijlee2@users.noreply.github.com>
2021-01-24 11:06:46 +0100

    Extracted <Mascots::MascotFilters> component from <TomsterList>

---
+++ tests/integration/components/mascots/mascot-filters-test.js
@@ -0,0 +1,97 @@
+import { findAll, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupRenderingTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+
+module('Integration | Component | mascots/mascot-filters', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('We show 9 filters', async function (assert) {
+    /*
+      TODO:
+
+      After we upgrade ember-source to v3.24+, we can remove
+      the line `this.owner.setupRouter();`.
+    */
+    this.owner.setupRouter();
+
+    /* BEGIN: Set up custom assertions */
+    assert.areLinksCorrect = (expectedOutput = []) => {
+      const links = findAll('[data-test-link]');
+
+      assert.strictEqual(
+        links.length,
+        expectedOutput.length,
+        `The user sees ${expectedOutput.length} links.`
+      );
+
+      if (links.length !== expectedOutput.length) {
+        return;
+      }
+
+      links.forEach((link, index) => {
+        assert
+          .dom(link)
+          .hasAttribute(
+            'href',
+            expectedOutput[index].href,
+            `The URL for the link is correct. (${index + 1})`
+          )
+          .hasText(
+            expectedOutput[index].label,
+            `The label for the link is correct. (${index + 1})`
+          );
+      });
+    };
+    /* END: Set up custom assertions */
+
+    /* BEGIN: Test */
+    await render(hbs`
+      <Mascots::MascotFilters />
+    `);
+
+    assert.areLinksCorrect([
+      {
+        href: '/mascots',
+        label: 'All',
+      },
+      {
+        href: '/mascots?filter=tomster',
+        label: 'Tomsters',
+      },
+      {
+        href: '/mascots?filter=zoey',
+        label: 'Zoeys',
+      },
+      {
+        href: '/mascots?filter=meetup',
+        label: 'Meetups',
+      },
+      {
+        href: '/mascots?filter=conference',
+        label: 'Conferences',
+      },
+      {
+        href: '/mascots?filter=corp',
+        label: 'Corporate',
+      },
+      {
+        href: '/mascots?filter=other',
+        label: 'Others',
+      },
+      {
+        href: '/mascots',
+        label: 'Small',
+      },
+      {
+        href: '/mascots?display=large',
+        label: 'Large',
+      },
+    ]);
+    /* END: Test */
+
+    /* BEGIN: Clean up custom assertions */
+    delete assert.areLinksCorrect;
+    /* END: Clean up custom assertions */
+  });
+});
