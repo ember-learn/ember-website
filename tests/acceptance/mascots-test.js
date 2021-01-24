f6a3d37c8e5b3da6577134f529b46beece94e5fd
Isaac Lee <ijlee2@users.noreply.github.com>
2021-01-24 13:01:10 +0100

    Added application tests for mascots.index page

--- tests/acceptance/mascots-test.js
+++ tests/acceptance/mascots-test.js
@@ -1,4 +1,4 @@
-import { visit } from '@ember/test-helpers';
+import { click, currentURL, findAll, visit } from '@ember/test-helpers';
 import percySnapshot from '@percy/ember';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 import { setupMirage } from 'ember-cli-mirage/test-support';
@@ -28,5 +28,115 @@ module('Acceptance | mascots', function (hooks) {
     await a11yAudit();
 
     assert.hasPageTitle('Mascots - Ember.js');
+  });
+
+  test('A user can visit the Mascots page', async function (assert) {
+    await visit('/mascots');
+
+    assert.strictEqual(currentURL(), '/mascots', 'The URL is correct.');
+
+    const mascots = findAll('[data-test-mascot]');
+
+    assert.strictEqual(mascots.length, 10, 'The user sees 10 mascots.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[0])
+      .hasText('Austin Zoey', 'The user sees the correct first mascot.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[mascots.length - 1])
+      .hasText('Teaching Tomster', 'The user sees the correct last mascot.');
+  });
+
+  test('A user can filter mascots', async function (assert) {
+    await visit('/mascots');
+    await click('[data-test-link="Tomsters"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/mascots?filter=tomster',
+      'The URL is correct.'
+    );
+
+    let mascots = findAll('[data-test-mascot]');
+
+    assert.strictEqual(mascots.length, 7, 'The user sees 7 mascots.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[0])
+      .hasText('EmberCamp Tomster', 'The user sees the correct first mascot.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[mascots.length - 1])
+      .hasText('Teaching Tomster', 'The user sees the correct last mascot.');
+
+    // Try another filter
+    await click('[data-test-link="Zoeys"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/mascots?filter=zoey',
+      'The URL is correct.'
+    );
+
+    mascots = findAll('[data-test-mascot]');
+
+    assert.strictEqual(mascots.length, 6, 'The user sees 6 mascots.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[0])
+      .hasText('Austin Zoey', 'The user sees the correct first mascot.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[mascots.length - 1])
+      .hasText('Skylight', 'The user sees the correct last mascot.');
+  });
+
+  test('A user can change the size of mascots', async function (assert) {
+    await visit('/mascots');
+    await click('[data-test-link="Tomsters"]');
+    await click('[data-test-link="Large"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/mascots?display=large&filter=tomster',
+      'The URL is correct.'
+    );
+
+    let mascots = findAll('[data-test-mascot]');
+
+    assert.strictEqual(mascots.length, 7, 'The user sees 7 mascots.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[0])
+      .hasText('EmberCamp Tomster', 'The user sees the correct first mascot.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[mascots.length - 1])
+      .hasText('Teaching Tomster', 'The user sees the correct last mascot.');
+
+    await percySnapshot(assert);
+
+    // Try another filter
+    await click('[data-test-link="Zoeys"]');
+    await click('[data-test-link="Small"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/mascots?filter=zoey',
+      'The URL is correct.'
+    );
+
+    mascots = findAll('[data-test-mascot]');
+
+    assert.strictEqual(mascots.length, 6, 'The user sees 6 mascots.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[0])
+      .hasText('Austin Zoey', 'The user sees the correct first mascot.');
+
+    assert
+      .dom('[data-test-field="Name"]', mascots[mascots.length - 1])
+      .hasText('Skylight', 'The user sees the correct last mascot.');
   });
 });
