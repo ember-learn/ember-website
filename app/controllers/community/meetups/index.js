7c231c7d529e2b2bdc3b31434ba519d4762e4645
Isaac Lee <ijlee2@users.noreply.github.com>
2021-01-24 14:56:49 +0100

    Replaced @groupBy decorator with a getter

--- app/controllers/community/meetups/index.js
+++ app/controllers/community/meetups/index.js
@@ -1,17 +1,37 @@
-import Controller from '@ember/controller';
-import groupBy from 'ember-group-by';
-import { inject as service } from '@ember/service';
 import { getOwner } from '@ember/application';
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class CommunityMeetupsIndexController extends Controller {
   @service fastboot;
 
-  @groupBy('model', 'area') meetupsByArea;
   @tracked leafletPackageLoaded = false;
   @tracked lat = 20;
   @tracked lng = 0;
   @tracked zoom = 2;
+
+  get meetupsByArea() {
+    const meetups = (this.model ?? []).toArray();
+    const groupMeetupsByArea = new Map();
+
+    meetups.forEach((meetup) => {
+      const { area } = meetup;
+
+      if (groupMeetupsByArea.has(area)) {
+        let { items } = groupMeetupsByArea.get(area);
+        items.push(meetup);
+      } else {
+        groupMeetupsByArea.set(area, {
+          items: [meetup],
+          property: 'area',
+          value: area,
+        });
+      }
+    });
+
+    return Array.from(groupMeetupsByArea.values());
+  }
 
   constructor() {
     super(...arguments);
