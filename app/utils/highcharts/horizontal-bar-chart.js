90a9cb770240ac9a0bfe9b99310f09b3954b7d87
Isaac Lee <ijlee2@users.noreply.github.com>
2021-02-28 09:14:59 +0100

    Created a draft of highchartsOptions

--- app/utils/highcharts/horizontal-bar-chart.js
+++ app/utils/highcharts/horizontal-bar-chart.js
@@ -1,4 +1,37 @@
 /*
   https://api.highcharts.com/highcharts/plotOptions.bar
 */
-export default class HorizontalBarChart {}
+export default class HorizontalBarChart {
+  get highchartsOptions() {
+    return {
+      data: [],
+
+      options: {
+        chart: {
+          type: 'bar',
+        },
+
+        subtitle: {
+          text: 'TODO: Allow subtitle',
+        },
+
+        title: {
+          text: 'TODO: Allow title',
+        },
+
+        tooltip: {
+          enabled: true,
+          valueSuffix: '%',
+        },
+
+        xAxis: {
+          /* TODO */
+        },
+
+        yAxis: {
+          /* TODO */
+        },
+      },
+    };
+  }
+}
--- tests/unit/utils/highcharts/horizontal-bar-chart-test.js
+++ tests/unit/utils/highcharts/horizontal-bar-chart-test.js
@@ -8,5 +8,36 @@ module('Unit | Utility | highcharts/horizontal-bar-chart', function () {
 
       assert.ok(barChart, 'We can create an instance of HorizontalBarChart.');
     });
+
+    test('highchartsOptions returns an options object', function (assert) {
+      const { options } = new HorizontalBarChart().highchartsOptions;
+
+      assert.deepEqual(
+        options,
+        {
+          chart: {
+            type: 'bar',
+          },
+
+          subtitle: {
+            text: 'TODO: Allow subtitle',
+          },
+
+          title: {
+            text: 'TODO: Allow title',
+          },
+
+          tooltip: {
+            enabled: true,
+            valueSuffix: '%',
+          },
+
+          xAxis: {},
+
+          yAxis: {},
+        },
+        'We get the correct value.'
+      );
+    });
   });
 });
