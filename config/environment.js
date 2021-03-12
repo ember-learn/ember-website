5edaed35db03db58321b7f0597834b7bd2fa25c2
Amy Lam <amy.r.lam@gmail.com>
2021-03-12 11:29:07 -0800

    Update host so that images will show up on Twitter
    Relates to: same change done on ember-blog https://github.com/ember-learn/ember-blog/pull/873

--- config/environment.js
+++ config/environment.js
@@ -78,6 +78,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.APP.host = 'https://emberjs.com'; // For twitter:image etc to have full URL 
   }
 
   return ENV;
