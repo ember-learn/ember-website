ae23e69aac07dda4031f0a4459a5d0c467c1a895
fozy81 <fozy81@hotmail.com>
2021-03-24 15:52:07 +0000

    issue-788 remove 3.16 LTS version

--- app/controllers/releases/lts.js
+++ app/controllers/releases/lts.js
@@ -10,9 +10,5 @@ export default class ReleasesLtsController extends Controller {
       version: '3.20',
       promotionDate: new Date('August 24, 2020'),
     },
-    {
-      version: '3.16',
-      promotionDate: new Date('March 4, 2020'),
-    },
   ];
 }
