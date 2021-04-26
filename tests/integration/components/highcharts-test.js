a5a4c40b4e60dbf60c8a1adadea95b4f2dffc9a7
Isaac Lee <ijlee2@users.noreply.github.com>
2021-04-26 09:23:30 +0200

    Added a sample chart to rendering tests for <Highcharts> component

--- tests/integration/components/highcharts-test.js
+++ tests/integration/components/highcharts-test.js
@@ -2,14 +2,49 @@ import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupRenderingTest } from 'ember-qunit';
 import { waitUntilAllChartsAreDrawn } from 'ember-website/tests/helpers/highcharts';
+import VerticalBarChart from 'ember-website/utils/highcharts/vertical-bar-chart';
 import { module, test } from 'qunit';
 
 module('Integration | Component | highcharts', function (hooks) {
   setupRenderingTest(hooks);
 
+  hooks.beforeEach(function () {
+    this.chart = new VerticalBarChart({
+      chart: {
+        categories: ['Beginner', 'Intermediate', 'Advanced'],
+        title: 'Rank your web skills',
+      },
+
+      rawData: [
+        {
+          color: '#1A5E9A',
+          label: 'ARIA',
+          values: [68.3, 25.7, 6],
+        },
+        {
+          color: '#32AADE',
+          label: 'CSS',
+          values: [9.6, 47.0, 43.4],
+        },
+        {
+          color: '#F2682A',
+          label: 'HTML',
+          values: [1.9, 36.0, 62.1],
+        },
+        {
+          color: '#F1BF28',
+          label: 'JavaScript',
+          values: [2.7, 35.9, 61.4],
+        },
+      ],
+    });
+  });
+
   test('We can dynamically import Highcharts', async function (assert) {
     await render(hbs`
-      <Highcharts />
+      <Highcharts
+        @chart={{this.chart}}
+      />
     `);
 
     await waitUntilAllChartsAreDrawn();
