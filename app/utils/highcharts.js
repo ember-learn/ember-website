214ca5643f2763acc91de98f9bcd87a214c540a4
Srihari K <srihari.kookal@clark.de>
2021-05-13 11:36:39 +0200

    Lint fixes: Remove unused vars

--- app/utils/highcharts.js
+++ app/utils/highcharts.js
@@ -1,31 +1,8 @@
-import { assign } from '@ember/polyfills';
-import merge from 'deepmerge';
 import AreaSplineChart from 'ember-website/utils/highcharts/area-spline-chart';
 import HorizontalBarChart from 'ember-website/utils/highcharts/horizontal-bar-chart';
 import PieChart from 'ember-website/utils/highcharts/pie-chart';
 import SplineChart from 'ember-website/utils/highcharts/spline-chart';
 import VerticalBarChart from 'ember-website/utils/highcharts/vertical-bar-chart';
-
-const defaultChartOptions = {
-  chart: {
-    backgroundColor: 'transparent',
-    type: 'column',
-  },
-
-  title: {
-    text: '',
-  },
-
-  yAxis: {
-    labels: {
-      format: '{value} %',
-    },
-
-    title: {
-      text: null,
-    },
-  },
-};
 
 const highchartsOptions = {
   credits: {
--- tests/unit/utils/highcharts-test.js
+++ tests/unit/utils/highcharts-test.js
@@ -1,55 +1,6 @@
 import { highchartsOptions } from 'ember-website/utils/highcharts';
 import { module, test } from 'qunit';
 
-const customData = [
-  {
-    color: '#1E719B',
-    data: [
-      [1, 39],
-      [2, 19],
-      [3, 34],
-      [4, 36],
-      [5, 36],
-      [6, 67],
-      [7, 148],
-      [8, 278],
-      [9, 186],
-      [10, 384],
-    ],
-    name: '2019',
-    showInLegend: false,
-  },
-];
-
-const defaultData = [
-  {
-    color: '#aaaaaa',
-    data: 0,
-    id: 'placeholder',
-  },
-];
-
-const defaultChartOptions = {
-  chart: {
-    backgroundColor: 'transparent',
-    type: 'column',
-  },
-
-  title: {
-    text: '',
-  },
-
-  yAxis: {
-    labels: {
-      format: '{value} %',
-    },
-
-    title: {
-      text: null,
-    },
-  },
-};
-
 module('Unit | Utility | highcharts', function () {
   module('highchartsOptions', function () {
     test('returns an object with valid Highcharts configuration options', function (assert) {
