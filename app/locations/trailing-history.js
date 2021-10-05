6305a8a5dbbcc72e85a123de52722dec4f2a3343
fozy81 <fozy81@hotmail.com>
2021-10-05 20:57:24 +0100

    fix issue#862 - extract conditional into utility function

--- app/locations/trailing-history.js
+++ app/locations/trailing-history.js
@@ -1,16 +1,10 @@
 import HistoryLocation from '@ember/routing/history-location';
+import { formatURLConditional } from 'ember-website/utils/format-url';
 
 export default class TrailingHistoryLocation extends HistoryLocation {
   formatURL() {
     let url = super.formatURL(...arguments);
 
-    if (url.includes('#')) {
-      return url.replace(/([^/])#(.*)/, '$1/#$2');
-    }
-    if (url.includes('?')) {
-      return url.replace(/\/?$/, '');
-    } else {
-      return url.replace(/\/?$/, '/');
-    }
+    return formatURLConditional(url);
   }
 }
---
+++ app/utils/format-url.js
@@ -0,0 +1,10 @@
+export function formatURLConditional(url) {
+  if (url.includes('#')) {
+    return url.replace(/([^/])#(.*)/, '$1/#$2');
+  }
+  if (url.includes('?')) {
+    return url.replace(/\/?$/, '');
+  } else {
+    return url.replace(/\/?$/, '/');
+  }
+}
