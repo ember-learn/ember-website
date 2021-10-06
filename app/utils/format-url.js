05d3cd0919f90fa5ecb7248379fb466fa2b510a6
Tim <fozy81@hotmail.com>
2021-10-06 08:34:53 +0100

    Update app/utils/format-url.js
    Co-authored-by: Isaac Lee <16869656+ijlee2@users.noreply.github.com>

--- app/utils/format-url.js
+++ app/utils/format-url.js
@@ -2,9 +2,10 @@ export function formatURLConditional(url) {
   if (url.includes('#')) {
     return url.replace(/([^/])#(.*)/, '$1/#$2');
   }
+
   if (url.includes('?')) {
     return url.replace(/\/?$/, '');
-  } else {
-    return url.replace(/\/?$/, '/');
   }
+
+  return url.replace(/\/?$/, '/');
 }
