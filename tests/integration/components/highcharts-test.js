2f60e62c0c644dc6aca26580d4de77324c4809f8
ijlee2 <ijlee2@users.noreply.github.com>
2022-05-19 08:04:17 +0200

    refactor: Updated the <Highcharts> component to use the {{draw-chart}} modifier

--- tests/integration/components/highcharts-test.js
+++ tests/integration/components/highcharts-test.js
@@ -1,46 +1,20 @@
 import { render } from '@ember/test-helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupRenderingTest } from 'ember-qunit';
 import { waitUntilAllChartsAreDrawn } from 'ember-website/tests/helpers/highcharts';
-import VerticalBarChart from 'ember-website/utils/highcharts/vertical-bar-chart';
 import { module, test } from 'qunit';
 
 module('Integration | Component | highcharts', function (hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function () {
-    this.chart = new VerticalBarChart({
-      chart: {
-        categories: ['Beginner', 'Intermediate', 'Advanced'],
-        title: 'Rank your web skills',
-      },
-
-      rawData: [
-        {
-          color: '#1A5E9A',
-          label: 'ARIA',
-          values: [68.3, 25.7, 6],
-        },
-        {
-          color: '#32AADE',
-          label: 'CSS',
-          values: [9.6, 47.0, 43.4],
-        },
-        {
-          color: '#F2682A',
-          label: 'HTML',
-          values: [1.9, 36.0, 62.1],
-        },
-        {
-          color: '#F1BF28',
-          label: 'JavaScript',
-          values: [2.7, 35.9, 61.4],
-        },
-      ],
-    });
+    this.chart = {
+      highchartsOptions: {},
+    };
   });
 
-  test('We can dynamically import Highcharts', async function (assert) {
+  test('The component renders an svg chart', async function (assert) {
     await render(hbs`
       <Highcharts
         @chart={{this.chart}}
@@ -50,7 +24,11 @@ module('Integration | Component | highcharts', function (hooks) {
     await waitUntilAllChartsAreDrawn();
 
     assert
-      .dom('[data-test-highcharts-container] svg')
+      .dom('[data-test-chart] svg')
       .exists({ count: 1 }, 'We see an svg element.');
+
+    await a11yAudit();
+
+    assert.ok(true, 'We passed the accessibility audit.');
   });
 });
