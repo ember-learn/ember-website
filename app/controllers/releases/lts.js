1d4f4db305e1a9578b98c3731cb27d20b2b69de2
Jared Galanis <jaredgalanis@users.noreply.github.com>
2022-12-08 23:10:00 -0500

    update lts page

--- app/controllers/releases/lts.js
+++ app/controllers/releases/lts.js
@@ -3,12 +3,12 @@ import Controller from '@ember/controller';
 export default class ReleasesLtsController extends Controller {
   currentlySupportedLTS = [
     {
-      version: '4.4',
-      promotionDate: new Date('July 13, 2022'),
+      version: '4.8',
+      promotionDate: new Date('November 28, 2022'),
     },
     {
-      version: '3.28',
-      promotionDate: new Date('December 20, 2021'),
+      version: '4.4',
+      promotionDate: new Date('July 13, 2022'),
     },
   ];
 }
