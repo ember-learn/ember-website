cd0d40cfdb5b08b4d0bc5a92103198bb76cd99b6
Chris Manson <chris@manson.ie>
2023-06-08 10:32:14 +0100

    add embroider initiative to the sponsors page

---
+++ app/models/initiative-sponsor.js
@@ -0,0 +1,9 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class SponsorModel extends Model {
+  @attr content;
+  @attr image;
+  @attr name;
+  @attr url;
+  @attr('number') tier;
+}
