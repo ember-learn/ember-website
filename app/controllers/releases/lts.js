3781f15f2e812b56dab221a4be523ef501f25c1f
Chris Manson <chris@manson.ie>
2023-06-22 21:43:31 +0100

    add LTS info to the table

--- app/controllers/releases/lts.js
+++ app/controllers/releases/lts.js
@@ -1,14 +1,20 @@
 import Controller from '@ember/controller';
 
 export default class ReleasesLtsController extends Controller {
+  // promotion date should be the day of the release of the following ember minor version
+  // you can run `npm info ember-source time` to see a list
   currentlySupportedLTS = [
     {
+      version: '4.12',
+      promotionDate: new Date('2023-05-15'),
+    },
+    {
       version: '4.8',
-      promotionDate: new Date('November 28, 2022'),
+      promotionDate: new Date('2022-11-28'),
     },
     {
       version: '4.4',
-      promotionDate: new Date('July 13, 2022'),
+      promotionDate: new Date('2022-07-13'),
     },
   ];
 }
