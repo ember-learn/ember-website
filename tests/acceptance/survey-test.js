9ae357f09e297e7081899917b91f69b49573e430
Ignace Maes <contact@ignacemaes.com>
2024-04-20 16:50:20 +0200

    feat: add survey 2024 page

--- tests/acceptance/survey-test.js
+++ tests/acceptance/survey-test.js
@@ -13,8 +13,8 @@ module('Acceptance | survey', function (hooks) {
   test('When a user visits /survey, we redirect them to the most recent survey page', async function (assert) {
     await visit('/survey');
 
-    assert.strictEqual(currentURL(), '/survey/2022', 'The URL is correct.');
+    assert.strictEqual(currentURL(), '/survey/2024', 'The URL is correct.');
 
-    assert.hasPageTitle('Ember Community Survey 2022 - Ember.js');
+    assert.hasPageTitle('Ember Community Survey 2024 - Ember.js');
   });
 });
