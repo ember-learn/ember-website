83b8f6415487f6d8bd33819ddc6952aaf120cd6a
Ignace Maes <ignacemaes100@msn.com>
2024-05-01 08:18:29 +0200

    Update tests/unit/controllers/teams-test.js

--- tests/unit/controllers/teams-test.js
+++ tests/unit/controllers/teams-test.js
@@ -65,7 +65,11 @@ module('Unit | Controller | teams', function (hooks) {
     // Create an intermediate data structure for assertion
     const output = dataTeamMembers.map((teamMember) => teamMember.id);
 
-    assert.deepEqual(output, ['chris-thoburn', 'krystan-huffmenne', 'rich-glazerman']);
+    assert.deepEqual(output, [
+      'chris-thoburn',
+      'krystan-huffmenne',
+      'rich-glazerman',
+    ]);
   });
 
   test('We sort Learning team members by when they were first added', function (assert) {
