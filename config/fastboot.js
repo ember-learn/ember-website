83eddb03d1abb11fd0ff4563056cb4de702c78d8
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-04 13:11:17 -0400

    Upgrade to ember-source 5.8 / ember-data 4.12

---
+++ config/fastboot.js
@@ -0,0 +1,24 @@
+module.exports = function () {
+  return {
+    buildSandboxGlobals(defaultGlobals) {
+      return Object.assign({}, defaultGlobals, {
+        fetch,
+        AbortController,
+        crypto: require('node:crypto'),
+        ReadableStream:
+          typeof ReadableStream !== 'undefined'
+            ? ReadableStream
+            : require('node:stream/web').ReadableStream,
+        WritableStream:
+          typeof WritableStream !== 'undefined'
+            ? WritableStream
+            : require('node:stream/web').WritableStream,
+        TransformStream:
+          typeof TransformStream !== 'undefined'
+            ? TransformStream
+            : require('node:stream/web').TransformStream,
+        Headers: typeof Headers !== 'undefined' ? Headers : undefined,
+      });
+    },
+  };
+};
