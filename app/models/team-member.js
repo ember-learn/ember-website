b74d069adb66c27fdcf759c45010721db39fe48d
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2025-05-05 11:23:23 +0200

    Add Bluesky & Mastodon support

--- app/models/team-member.js
+++ app/models/team-member.js
@@ -2,10 +2,12 @@ import Model, { attr } from '@ember-data/model';
 
 export default class TeamMemberModel extends Model {
   @attr('date') added;
+  @attr bluesky;
   @attr first;
   @attr github;
   @attr image;
   @attr last;
+  @attr mastodon;
   @attr name;
   @attr teams;
   @attr twitter;
