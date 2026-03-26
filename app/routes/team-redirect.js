d667da9ea7f3823b345cda4e1148f7914469498b
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-03-26 20:52:17 +0100

    Resolve Ember v4 deprecations

--- app/routes/team-redirect.js
+++ app/routes/team-redirect.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class TeamRedirectRoute extends Route {
+  @service router;
+
   redirect() {
-    this.transitionTo('teams');
+    this.router.transitionTo('teams');
   }
 }
