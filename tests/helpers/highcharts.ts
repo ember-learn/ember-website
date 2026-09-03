b080b2539915f2dd9edad4b06389832fe05c9385
ijlee2 <ijlee2@users.noreply.github.com>
2026-09-03 09:34:53 +0200

    chore: Updated project configurations

--- tests/helpers/highcharts.ts
+++ tests/helpers/highcharts.ts
@@ -1,10 +1,12 @@
 import { findAll, waitUntil } from '@ember/test-helpers';
 
-export async function waitUntilAllChartsAreDrawn() {
+export async function waitUntilAllChartsAreDrawn(): Promise<void> {
   await waitUntil(() => {
     let areAllChartsSettled = true;
 
-    findAll('[data-test-chart]').forEach(({ dataset }) => {
+    findAll('[data-test-chart]').forEach((element) => {
+      const { dataset } = element as HTMLElement;
+
       if (dataset.renderState !== 'settled') {
         areAllChartsSettled = false;
       }
